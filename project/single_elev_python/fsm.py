9c5c93568fb93c7798c5b48d92c61ef1d6e5f40d
Knut V Eriksen <knutve@stud.ntnu.no>
2020-02-23 15:12:26 +0100

    Cost function start

--- project/single_elev_python/fsm.py
+++ project/single_elev_python/fsm.py
@@ -5,7 +5,6 @@ import status
 import util
 import timer
 
-elev = LocalElevator()
 
 self = cdll.LoadLibrary("./driver.so")
 
@@ -15,52 +14,52 @@ self = cdll.LoadLibrary("./driver.so")
 #    do shit here
 
 #Denne burde kanskje flyttes til et annet sted? Kanskje rename util til utils, og legge til denne der?
-def fsm_setAllLights(): #Call this function when new order is done, as callback, or distributed
+def fsm_setAllLights(e): #Call this function when new order is done, as callback, or distributed
     for f in range(N_FLOORS):
         for b in range(N_BUTTONS):
             #for id in id_list: - Implementer dette
-                if (elev.queue[id][f][b]):
+                if (e.queue[id][f][b]):
                     self.elev_set_button_lamp(b, f, 1)
                     break
                 self.elev_set_button_lamp(b, f, 0)
 
     return
 
-def fsm_onInitBetweenFloors():
+def fsm_onInitBetweenFloors(e):
     self.elev_set_motor_direction(DIRN_DOWN)
-    elev.direction =DIRN_DOWN
-    elev.behaviour = MOVING
+    e.direction =DIRN_DOWN
+    e.behaviour = MOVING
     return
 
-def fsm_onNewOrder(id,f,b): #When a new order is distributed and confirmed from the cost function
-    elev.queue[id][f][b] = 1
-    fsm_setAllLights()
+def fsm_onNewOrder(e,f,b): #When a new order is distributed and confirmed from the cost function
+    e.queue[e.id][f][b] = 1
+    fsm_setAllLights(e)
     return
 
-def fsm_onFloorArrival(f):
-    elev.floor = f
+def fsm_onFloorArrival(e,f):
+    e.floor = f
     self.elev_set_floor_indicator(f)
-    if (elev.behaviour == MOVING):
-        if (util_shouldStop(elev)):
+    if (e.behaviour == MOVING):
+        if (util_shouldStop(e)):
             self.elev_set_motor_direction(DIRN_STOP)
             #publish order done message
                 #Legge til i callback att lyset for denne etsjen skrur seg av
             self.elev_set_door_open_lamp(1)
             timer_start()
-            util_clearAtCurrentFloor(elev)
-            fsm_setAllLights()
+            e = util_clearAtCurrentFloor(e)
+            fsm_setAllLights(e)
             elevator.behaviour = DOOR_OPEN
     return
 
-def fsm_onDoorTimeout():
+def fsm_onDoorTimeout(e):
     bh = elevator.behaviour
     if (bh == DOOR_OPEN):
-        elev.dirn = util_chooseDirection(elev)
+        e.dirn = util_chooseDirection(e)
         self.elev_set_door_open_lamp(0)
-        self.elev_set_motor_direction(elev.direction)
+        self.elev_set_motor_direction(e.direction)
 
-        if (elev.direction == DIRN_STOP):
-            elev.behaviour = IDLE
+        if (e.direction == DIRN_STOP):
+            e.behaviour = IDLE
         else:
-            elev.behaviour = MOVING
+            e.behaviour = MOVING
     return
