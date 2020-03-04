6e2bfc4bd95fa84f3035c981f208d6a4e6e69a21
csletten <c.sletten99@gmail.com>
2020-03-04 09:49:38 +0100

    Dicts working

--- project/single_elev_python/util.py
+++ project/single_elev_python/util.py
@@ -38,18 +38,18 @@ def util_chooseDirection(e):
 def util_shouldStop(e):
     dir = e.direction[e.id]
     if (dir == constants.DIRN_DOWN):
-        return ((e.queue[e.id][e.floor][constants.BTN_HALL_DOWN]) or
-                (e.queue[e.id][e.floor][constants.BTN_CAB]) or
+        return ((e.queue[e.id][e.floor[e.id]][constants.BTN_HALL_DOWN]) or
+                (e.queue[e.id][e.floor[e.id]][constants.BTN_CAB]) or
                 not(util_orderBelow(e)))
     elif (dir == constants.DIRN_UP):
-        return ((e.queue[e.id][e.floor][constants.BTN_HALL_UP]) or
-                (e.queue[e.id][e.floor][constants.BTN_CAB]) or
+        return ((e.queue[e.id][e.floor[e.id]][constants.BTN_HALL_UP]) or
+                (e.queue[e.id][e.floor[e.id]][constants.BTN_CAB]) or
                 not(util_orderAbove(e)))
     else:
         return 1
 
 def util_clearAtCurrentFloor(e):
     for b in range(constants.N_BUTTONS):
-        if (e.queue[e.id][e.floor][b]):
-            e.queue[e.id][e.floor][b] = 0
+        if (e.queue[e.id][e.floor[e.id]][b]):
+            e.queue[e.id][e.floor[e.id]][b] = 0
     return e
